99d322915f07c9c1c8a2cab7c194f961d2d7677c
ViktorC <viktor.csomor@gmail.com>
2018-03-13 12:00:28 +0100

    Regenerated Doxygen documentation.

--- docs/html/search/all_e.js
+++ docs/html/search/all_e.js
@@ -13,10 +13,5 @@ var searchData=
   ['norm_5favg_5fdecay',['norm_avg_decay',['../classcattle_1_1_batch_norm_layer_base.html#aa103f525dbd5e600097438873f1c4ec5',1,'cattle::BatchNormLayerBase']]],
   ['normalizationpreprocessor',['NormalizationPreprocessor',['../classcattle_1_1_normalization_preprocessor.html',1,'cattle::NormalizationPreprocessor&lt; Scalar, Rank &gt;'],['../classcattle_1_1_normalization_preprocessor.html#a90c8928923e3bb85b175c89507a9a67c',1,'cattle::NormalizationPreprocessor::NormalizationPreprocessor()'],['../classcattle_1_1_normalization_preprocessor_3_01_scalar_00_013_01_4.html#af47df55ba3f0fd0d18586d87019a7f48',1,'cattle::NormalizationPreprocessor&lt; Scalar, 3 &gt;::NormalizationPreprocessor()']]],
   ['normalizationpreprocessor_3c_20scalar_2c_203_20_3e',['NormalizationPreprocessor&lt; Scalar, 3 &gt;',['../classcattle_1_1_normalization_preprocessor_3_01_scalar_00_013_01_4.html',1,'cattle']]],
-  ['null_5fmatrix',['NULL_MATRIX',['../classcattle_1_1_utils.html#afa7c951b4147c3fd6f7efebc769139f4',1,'cattle::Utils']]],
-  ['null_5ftensor2',['NULL_TENSOR2',['../classcattle_1_1_utils.html#a16edf68e21c1cbd4eed55d3c54bfeacd',1,'cattle::Utils']]],
-  ['null_5ftensor3',['NULL_TENSOR3',['../classcattle_1_1_utils.html#a830dd5bd4a7b96c22821a775eb10511b',1,'cattle::Utils']]],
-  ['null_5ftensor4',['NULL_TENSOR4',['../classcattle_1_1_utils.html#a46829f4c2e1c13e29880e11edbaf2dc9',1,'cattle::Utils']]],
-  ['null_5ftensor5',['NULL_TENSOR5',['../classcattle_1_1_utils.html#a8e334664279631810703e87a4446da9d',1,'cattle::Utils']]],
   ['num_5fof_5feval_5fthreads',['num_of_eval_threads',['../classcattle_1_1_utils.html#a0a89bf03edc541e2aa59693ab39deb75',1,'cattle::Utils']]]
 ];
