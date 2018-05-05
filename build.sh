a7f4802b40b0a13151d8d4b8bf050d0648ce71d5
ViktorC <viktor.csomor@gmail.com>
2018-05-05 18:52:46 +0200

    Setting up multi-compiler Travis build.

--- build.sh
+++ build.sh
@@ -2,49 +2,34 @@
 
 if [ "$COMPILER_NAME" = "g++" ]; then
   make coverage
+elif [ "$COMPILER_NAME" = "clang++" ]; then
+  make clang_all
+else
+  echo "Invalid compiler"
+  exit 1
+fi
+rc=$?
+if [ "$rc" -ne 0 ]; then
+  echo "Build failed" >&2
+  exit "$rc"
+fi
+for run in {1.."$TRIES"}
+do
+  make check
   rc=$?
-  if [ "$rc" -ne 0 ]; then
-    echo "GCC build failed" >&2
-    exit "$rc"
+  if [ "$rc" -eq 0 ]; then
+    break
   fi
-  for run in {1.."$TRIES"}
-  do
-    make check
-    rc=$?
-    if [ "$rc" -eq 0 ]; then
-      break
-    fi
-  done
-  if [ "$rc" -ne 0 ]; then
-    echo "GCC test failed" >&2
-    exit "$rc"
-  fi
+done
+if [ "$rc" -ne 0 ]; then
+  echo "Test failed" >&2
+  exit "$rc"
+fi
+if [ "$COMPILER_NAME" = "g++" ]; then
   make report
   rc=$?
   if [ "$rc" -ne 0 ]; then
     echo "GNU Coverage report failed" >&2
     exit "$rc"
   fi
-elif [ "$COMPILER_NAME" = "clang++" ]; then
-  make clang_all
-  rc=$?
-  if [ "$rc" -ne 0 ]; then
-    echo "Clang build failed" >&2
-    exit "$rc"
-  fi
-  for run in {1.."$TRIES"}
-  do
-    make check
-    rc=$?
-    if [ "$rc" -eq 0 ]; then
-      break
-    fi
-  done
-  if [ "$rc" -ne 0 ]; then
-    echo "Clang test failed" >&2
-    exit "$rc"
-  fi
-else
-  echo 'Invalid compiler' >&2
-  exit 1
 fi
