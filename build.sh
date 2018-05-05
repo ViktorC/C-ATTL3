ae17d9cdb9545d84ad44c2ff128bc056bfeb30be
ViktorC <viktor.csomor@gmail.com>
2018-05-05 16:07:07 +0200

    Testing using both Clang and GCC.

---
+++ build.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+if [ $COMPILER_NAME -eq "g++" ]; then
+  make coverage
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "GCC build failed" >&2
+    exit $rc
+  fi
+  for run in {1..$GCC_TRIES}
+  do
+    make check
+    rc=$?
+    if [ $rc -eq 0 ]; then
+      break
+    fi
+  done
+  if [ $rc -ne 0 ]; then
+    echo "GCC test failed" >&2
+    exit $rc
+  fi
+  make report
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "GNU Coverage report failed" >&2
+    exit $rc
+  fi
+  bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
+elif [ $COMPILER_NAME -eq "clang++" ]; then
+  make clang_all
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "Clang build failed" >&2
+    exit $rc
+  fi
+  for run in {1..$CLANG_TRIES}
+  do
+    make check
+    rc=$?
+    if [ $rc -eq 0 ]; then
+      break
+    fi
+  done
+  if [ $rc -ne 0 ]; then
+    echo "Clang test failed" >&2
+    exit $rc
+  fi
+else
+  echo 'Invalid compiler' >&2
+  exit 1
+fi
