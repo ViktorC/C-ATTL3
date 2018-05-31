1a4a5edfa2335108aeb78f921a3209a322907004
ViktorC <viktor.csomor@gmail.com>
2018-06-01 00:17:54 +0200

    Fixed GAN example.
    Eigen's tensor module's normal random generator returns inf once in a
    while so I switched to uniform distribution.

--- examples/mnist_gan.cpp
+++ examples/mnist_gan.cpp
@@ -20,15 +20,19 @@ int main() {
 	MNISTDataProvider<float> real_file_train_prov(mnist_folder + "train-images.idx3-ubyte", mnist_folder + "train-labels.idx1-ubyte");
 	Tensor<float,4> mnist_train_data = real_file_train_prov.get_data(60000).first;
 	mnist_train_data = (mnist_train_data - mnist_train_data.constant(127.5)) / mnist_train_data.constant(127.5);
+	Tensor<float,4> mnist_train_label = Tensor<float,4>(60000u, 1u, 1u, 1u).random() * .1f;
+	mnist_train_label += mnist_train_label.constant(.9);
 	MemoryDataProvider<float,3,false> real_train_prov(
 			TensorPtr<float,4>(new Tensor<float,4>(std::move(mnist_train_data))),
-			TensorPtr<float,4>(new Tensor<float,4>(Tensor<float,4>(60000u, 1u, 1u, 1u).constant(1))));
+			TensorPtr<float,4>(new Tensor<float,4>(std::move(mnist_train_label))));
 	MNISTDataProvider<float> real_file_test_prov(mnist_folder + "t10k-images.idx3-ubyte", mnist_folder + "t10k-labels.idx1-ubyte");
 	Tensor<float,4> mnist_test_data = real_file_test_prov.get_data(10000).first;
 	mnist_test_data = (mnist_test_data - mnist_test_data.constant(127.5)) / mnist_test_data.constant(127.5);
+	Tensor<float,4> mnist_test_label = Tensor<float,4>(10000u, 1u, 1u, 1u).random() * .1f;
+	mnist_test_label += mnist_test_label.constant(.9);
 	MemoryDataProvider<float,3,false> real_test_prov(
 			TensorPtr<float,4>(new Tensor<float,4>(std::move(mnist_test_data))),
-			TensorPtr<float,4>(new Tensor<float,4>(Tensor<float,4>(10000u, 1u, 1u, 1u).constant(1))));
+			TensorPtr<float,4>(new Tensor<float,4>(std::move(mnist_test_label))));
 	// Create the GAN.
 	auto init = std::make_shared<GaussianWeightInitialization<float>>(2e-2);
 	float lrelu_alpha = .2;
@@ -94,7 +98,7 @@ int main() {
 			auto real_train_data = real_train_prov.get_data(k * m);
 			auto actual_m = real_train_data.first.dimension(0);
 			total_m += actual_m;
-			Tensor<float,4> train_data = gan.get_modules()[0]->infer(Tensor<float,4>(actual_m, 1u, 1u, 100u).template random<NormalRandGen<float>>())
+			Tensor<float,4> train_data = gan.get_modules()[0]->infer(Tensor<float,4>(actual_m, 1u, 1u, 100u).random())
 					.concatenate(std::move(real_train_data.first), 0);
 			Tensor<float,4> train_label = Tensor<float,4>(actual_m, 1u, 1u, 1u).constant(0).concatenate(std::move(real_train_data.second), 0);
 			MemoryDataProvider<float,3,false,false> disc_train_prov(
@@ -103,7 +107,7 @@ int main() {
 			disc_train_loss += disc_opt.train(*gan.get_modules()[1], disc_train_prov, 1, false) * actual_m;
 			// Then optimize the generator by maximizing the GAN's loss w.r.t. the parameters of the generator.
 			MemoryDataProvider<float,3,false,false> gen_train_prov(
-					TensorPtr<float,4>(new Tensor<float,4>(Tensor<float,4>(l * actual_m, 1u, 1u, 100u).template random<NormalRandGen<float>>())),
+					TensorPtr<float,4>(new Tensor<float,4>(Tensor<float,4>(l * actual_m, 1u, 1u, 100u).random())),
 					TensorPtr<float,4>(new Tensor<float,4>(Tensor<float,4>(l * actual_m, 1u, 1u, 1u).constant(1))));
 			gan.get_modules()[1]->set_frozen(true);
 			gen_train_loss += gen_opt.train(gan, gen_train_prov, 1, false) * actual_m;
@@ -116,7 +120,7 @@ int main() {
 		real_test_prov.reset();
 		auto real_test_data = real_test_prov.get_data();
 		auto actual_m = real_test_data.first.dimension(0);
-		Tensor<float,4> test_data = gan.get_modules()[0]->infer(Tensor<float,4>(actual_m, 1u, 1u, 100u).template random<NormalRandGen<float>>())
+		Tensor<float,4> test_data = gan.get_modules()[0]->infer(Tensor<float,4>(actual_m, 1u, 1u, 100u).random())
 				.concatenate(std::move(real_test_data.first), 0);
 		Tensor<float,4> test_label = Tensor<float,4>(actual_m, 1u, 1u, 1u).constant(0).concatenate(std::move(real_test_data.second), 0);
 		MemoryDataProvider<float,3,false,false> disc_test_prov(
@@ -125,7 +129,7 @@ int main() {
 		float disc_test_loss = disc_opt.test(*gan.get_modules()[1], disc_test_prov, false);
 		std::cout << "\tdiscriminator test loss: " << disc_test_loss << std::endl;
 		MemoryDataProvider<float,3,false,false> gen_test_prov(
-				TensorPtr<float,4>(new Tensor<float,4>(Tensor<float,4>(actual_m, 1u, 1u, 100u).template random<NormalRandGen<float>>())),
+				TensorPtr<float,4>(new Tensor<float,4>(Tensor<float,4>(actual_m, 1u, 1u, 100u).random())),
 				TensorPtr<float,4>(new Tensor<float,4>(Tensor<float,4>(actual_m, 1u, 1u, 1u).constant(1))));
 		gan.get_modules()[1]->set_frozen(true);
 		float gen_test_loss = gen_opt.test(gan, gen_test_prov, false);
@@ -133,7 +137,7 @@ int main() {
 		std::cout << "\tgenerator test loss: " << gen_test_loss << std::endl;
 		// Generate some fake image samples to track progress.
 		for (unsigned j = 0; j < 10; ++j) {
-			auto fake_image_sample = gan.get_modules()[0]->infer(Tensor<float,4>(1u, 1u, 1u, 100u).template random<NormalRandGen<float>>());
+			auto fake_image_sample = gan.get_modules()[0]->infer(Tensor<float,4>(1u, 1u, 1u, 100u).random());
 			fake_image_sample = (fake_image_sample + fake_image_sample.constant(1)) * fake_image_sample.constant(127.5);
 			ppm_codec.encode(TensorMap<float,3>(fake_image_sample.data(), { 28u, 28u, 1u }), std::string("image") +
 					std::to_string(i) + std::string("_") + std::to_string(j) + std::string(".pgm"));
